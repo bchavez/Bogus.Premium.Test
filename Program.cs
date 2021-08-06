840fe9daacd76bf9791c8bc7b0eeeb6d71503401
bchavez <bchavez@bitarmory.com>
2021-08-05 19:32:16 -0600

    Add Bogus.Healthcare. Add JSON output.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Bogus.Healthcare;
+using Newtonsoft.Json;
 using static System.Console;
 
 namespace Bogus.Premium.Test
@@ -7,9 +9,16 @@ namespace Bogus.Premium.Test
     {
         static void Main(string[] args)
         {
-            WriteLine("Hello Bogus!");
+            WriteLine("Hello Bogus Premium!");
 
-            var userFaker = new Faker<User>();
+            var userFaker = new Faker<User>()
+               .RuleFor(u => u.FirstName, f => f.Person.FirstName)
+               .RuleFor(u => u.PainArea, f => f.Human().BodyPartInternal());
+
+            var user = userFaker.Generate();
+
+            var json = JsonConvert.SerializeObject(user);
+            WriteLine(json);
         }
     }
 
@@ -18,5 +27,6 @@ namespace Bogus.Premium.Test
        public string FirstName { get; set; }
        public string LastName { get; set; }
        public int Age { get; set; }
+       public string PainArea { get; set; }
     }
 }
